c91e8b466ae8fac27ae264bac6ed3746faaa0a80
SaravananKM <117565384+SaravananKM@users.noreply.github.com>
2022-11-29 15:52:30 +0530

    Add files via upload

---
+++ assignments/week1/day1/Assignment2_TwoWheeler.java
@@ -0,0 +1,21 @@
+package assignments.week1.day1;
+
+public class Assignment2_TwoWheeler {
+	int noOfWheels=2;
+	short noOfMirrors=2;
+	long chassisNumber=2752533355577L;
+	boolean isPunctured=false;
+	String bikeName="Avenger 120 Street";
+	double runningKM=5300.35;
+		
+	public static void main(String[] args) {
+		Assignment2_TwoWheeler avn=new Assignment2_TwoWheeler();
+		System.out.println("Number Of Wheels: "+avn.noOfWheels);
+		System.out.println("Number Of Mirrors: "+avn.noOfMirrors);
+		System.out.println("Chassis Number: "+avn.chassisNumber);
+		System.out.println("Is it Punctured: "+avn.isPunctured);
+		System.out.println("Bike Name: "+avn.bikeName);
+		System.out.println("KM Ran: "+avn.runningKM);
+	}
+
+}
